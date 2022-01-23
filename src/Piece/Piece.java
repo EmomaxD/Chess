f7ee0c5278d883b1b2f1296e4170c9eba6ad6640
EmomaxD <emreleno@gmail.com>
2022-01-23 22:56:54 +0300

    Retyped the whole project

--- src/Piece/Piece.java
+++ src/Piece/Piece.java
@@ -1,14 +1,2 @@
-package Piece;
-
-import Board.Square;
-import Game.Color;
-
-public interface Piece {
-    Color Color();
-    boolean isWhite();
-    void MovePosition(int row,int col);
-    boolean checkLegalMove(int row,int col);
-    //need arrangement for possible moves (return type or think about inside)
-    Square[][] PossibleMoves();
+package Piece;public class Piece {
 }
-
